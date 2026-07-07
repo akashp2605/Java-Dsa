0b14125b958da989fa4e55d25f9332a18b3312ed
Akash <akashp2605@gmail.com>
2026-07-07 21:16:56 +0530

    Updated Arraylist Concepts Practice

--- ArraylistEx.java
+++ ArraylistEx.java
@@ -5,9 +5,21 @@ class ArraylistEx{
         list.add(20);
         list.add(90);
         list.add(30);
-        for(int n: list){
-            System.out.println(n);
-        }
+        list.add(40);
         System.out.println(list);
+        list.set(2,100 );
+       for (int i = 0; i < list.size(); i++) {
+    if (list.get(i) == 40) {
+        System.out.println("Found");
+        return;
     }
+}
+
+System.out.println("Not Found");
+        System.out.println(list);
+    //     for(int n: list){
+    //         System.out.println(n);
+    //     }
+    //     System.out.println(list);
+ }
 }
