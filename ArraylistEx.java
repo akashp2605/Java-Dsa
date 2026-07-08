49295384a305c8c689c0c72634e2c86a1770a58f
Akash <akashp2605@gmail.com>
2026-07-08 19:47:59 +0530

    Practiced problems on Arraylist

--- ArraylistEx.java
+++ ArraylistEx.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 class ArraylistEx{
     public static void main(String args[]){
-        ArrayList<Integer> list= new ArrayList<Integer>();
-        list.add(20);
-        list.add(90);
-        list.add(30);
-        list.add(40);
+        ArrayList<String> list= new ArrayList<String>();
+        list.add("Kane");
+        list.add("Messi");
+        list.add("Nani");
+        list.add("Ronaldo");
         System.out.println(list);
-        list.set(2,100 );
-       for (int i = 0; i < list.size(); i++) {
-    if (list.get(i) == 40) {
-        System.out.println("Found");
-        return;
-    }
-}
+        list.set(2,"India");
+        for (int i = 0; i < list.size(); i++) {
+        if (list.get(i).equals("India")) {
+            System.out.println("Found");
+            return;
+         }
+         }
 
-System.out.println("Not Found");
+        System.out.println("Not Found");
         System.out.println(list);
     //     for(int n: list){
     //         System.out.println(n);
