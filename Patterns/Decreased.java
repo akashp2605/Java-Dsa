3347d3ce2e33ca95520f845e709f21f16e0f221c
Akash <akashp2605@gmail.com>
2026-07-10 20:35:37 +0530

    Practiced Patterns

---
+++ Patterns/Decreased.java
@@ -0,0 +1,12 @@
+package Patterns;
+public class Decreased {
+    public static void main(String args[]){
+    int i,j;
+    for (i=10;i>=1;i--){
+        for(j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
