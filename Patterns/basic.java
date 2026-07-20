a8a19f96ce24f28dd8e06324a83da3f968ad1b8f
Akash <akashp2605@gmail.com>
2026-07-20 22:19:38 +0530

    Update loop limits in Basic class

--- Patterns/basic.java
+++ Patterns/basic.java
@@ -1,8 +1,8 @@
 package Patterns;
 class Basic {
     public static void main(String args[]){
-    for(int i=1;i<=4;i++){
-        for(int j=1;j<=5;j++){
+    for(int i=1;i<=9;i++){
+        for(int j=1;j<=8;j++){
             System.out.print("* ");
         }
         System.out.println();
