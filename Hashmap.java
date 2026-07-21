eff2d6d20661ea8489c465ea035d2be7b319f1ff
Akash <akashp2605@gmail.com>
2026-07-21 20:50:43 +0530

    Practiced Hashmap

---
+++ Hashmap.java
@@ -0,0 +1,12 @@
+public class Hashmap {
+    public static void main(String[] args) {
+        java.util.HashMap<Integer, String> map = new java.util.HashMap<>();
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        System.out.println(map.get(2));
+        System.out.println(map);
+        map.remove(1);
+        System.out.println(map);
+    }
+}
